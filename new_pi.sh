4e4b5c3adf0e2d02c1d2fa23b54372674b64eb4f
randalldigitalforge <randall@digitalforge-tek.com>
2024-03-01 19:36:19 -0800

    shell change moved to end of script

--- new_pi.sh
+++ new_pi.sh
@@ -6,15 +6,6 @@ sudo apt update && sudo apt upgrade -y
 # Install necessary packages
 sudo apt install -y zsh curl
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-
-# Change default shell to Zsh
-chsh -s $(which zsh)
-
-# Additional security measures
 # Enable firewall (ufw)
 sudo apt install -y ufw
 sudo ufw allow ssh
@@ -23,4 +14,6 @@ sudo ufw enable
 # Install fail2ban for additional security against brute-force attacks
 sudo apt install -y fail2ban
 
+# Install Oh My Zsh
+yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Setup completed. Please reboot your Raspberry Pi."
